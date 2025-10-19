bf4e220856be22dd083512fed2a40bbf67906ae0
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-19 11:00:10 -0300

    refactor: remoção da rota padrão do arquivo de rotas

--- CineBox/routes/web.php
+++ CineBox/routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::resource('movies', MovieController::class);
