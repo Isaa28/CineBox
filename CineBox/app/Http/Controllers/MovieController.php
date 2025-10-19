b42f7218af2a89e9fe7dba581acfb04bd842fba5
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-19 10:57:38 -0300

    feat: criação método index no MovieController

--- CineBox/app/Http/Controllers/MovieController.php
+++ CineBox/app/Http/Controllers/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
@@ -12,7 +13,8 @@ class MovieController extends Controller
      */
     public function index()
     {
-        //
+        $movies = Movie::all();  
+        return view('movies.index', compact('movies'));
     }
 
     /**
