081b7d31aee9ed57b1a1367b080228123e8b2301
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-26 15:22:12 -0300

    refactor: adicionando os controllers de rooms e sessions na rota

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RoomController;
 
 Route::resource('movies', MovieController::class);
 Route::resource('rooms', MovieController::class);
