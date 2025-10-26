d1c1a3904b66ca0bc6b446828d1c4209266aa064
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-26 16:20:12 -0300

    refactor: Modificação dos controlles das rotas.

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RoomController;
 
 Route::resource('movies', MovieController::class);
-Route::resource('rooms', MovieController::class);
-Route::resource('sessions', MovieController::class);
+Route::resource('rooms', RoomController::class);
+Route::resource('sessions', SessionController::class);
