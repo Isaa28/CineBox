53e8d9a90ac5eacb5bb2763ad7e9e443b5e1fdf0
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-26 15:01:25 -0300

    feat: criação das rotas rooms e sessions

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
 Route::resource('movies', MovieController::class);
+Route::resource('rooms', MovieController::class);
+Route::resource('sessions', MovieController::class);
