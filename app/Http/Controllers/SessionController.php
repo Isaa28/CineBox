5ef9e72dc3a13f4dca12352e22d72eb0b4ee7329
Isa <isadora.silva112@aluno.ce.gov.br>
2025-10-28 16:32:45 -0300

    feat: implementação do SessionController com métodos CRUD

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,62 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Session;
+use App\Models\Movie;
+use App\Models\Room;
 
-class SessionController extends Controller
+class sessionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
-        //
+        $sessions = Session::with(['movie', 'room'])->get();
+        return view('sessions.index', compact('sessions'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        $movies = Movie::all();
+        $rooms = Room::all();
+        return view('sessions.create', compact('movies', 'rooms'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        if(Session::create($request->all())) {
+            return redirect()->route('sessions.index')->with('sucesso', 'Sessão criada com sucesso!');    
+        }else{
+            return redirect()->route('sessions.index')->with('erro', 'Erro não foi possível cadastrar a sessão.');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        //
+        $session = Session::findOrFail($id);
+        return view('sessions.show', compact('session'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
+        $session = Session::findOrFail($id);
+        $movies = Movie::all();
+        $rooms = Room::all();
+        return view('sessions.edit', compact('session', 'movies', 'rooms'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $session = Session::findOrFail($id);
+        if($session->update($request->all())) {
+            return redirect()->route('sessions.index')->with('sucesso', 'Sessão atualizada com sucesso!');    
+        }else{
+            return redirect()->route('sessions.index')->with('erro', 'Erro não foi possível atualizar a sessão.');
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $session = Session::findOrFail($id);
+        if($session->delete()) {
+            return redirect()->back()->with('sucesso', 'Sessão deletada com sucesso!');    
+        }else{
+            return redirect()->back()->with('erro', 'Erro não foi possível deletar a sessão.');
+        }
     }
 }
