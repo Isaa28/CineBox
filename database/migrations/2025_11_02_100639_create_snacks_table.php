f04e32d4b21f55a0758881d2e58c98036b974faa
Isa <isadora.silva112@aluno.ce.gov.br>
2025-11-02 15:21:03 -0300

    feat: Criação das migrations Ticket e Snack.

---
+++ database/migrations/2025_11_02_100639_create_snacks_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('snacks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('type');
+            $table->decimal('price', 8, 2);
+            $table->integer('stock_quantity');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('snacks');
+    }
+};
