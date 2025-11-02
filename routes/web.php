ff3212f2da63e2505e1b3af7077bf1c58299fe99
Isa <isadora.silva112@aluno.ce.gov.br>
2025-11-02 16:19:36 -0300

    feat: criação das rotas de Ticket e Snack

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\SnackController;
 
 Route::resource('movies', MovieController::class);
 Route::resource('rooms', RoomController::class);
 Route::resource('sessions', SessionController::class);
+Route::resource('tickets', TicketController::class);
+Route::resource('snacks', SnackController::class);
