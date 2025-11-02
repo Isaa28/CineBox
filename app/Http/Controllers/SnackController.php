b52c12ed0395e217d69bc1b86571564f1a53cc6b
Isa <isadora.silva112@aluno.ce.gov.br>
2025-11-02 15:24:37 -0300

    feat: Criação e implementação dos Controlles Ticket e Snack.

---
+++ app/Http/Controllers/SnackController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Snack;
+
+class SnackController extends Controller
+{
+    public function index()
+    {
+        $snacks = Snack::all();  
+        return view('snacks.index', compact('snacks'));
+    }
+
+    public function create()
+    {
+        return view('snacks.create');
+    }
+
+    public function store(Request $request)
+    {
+        if(Snack::create($request->all())) {
+            return redirect()->route('snacks.index')->with('sucesso', 'Lanche criado com sucesso!');    
+        }else{
+            return redirect()->route('snacks.index')->with('erro', 'Erro não foi possível cadastrar o lanche.');
+        }
+    }
+
+    public function show(string $id)
+    {
+        $snack = Snack::findOrFail($id);
+        return view('snacks.show', compact('snack'));
+    }
+
+    public function edit(string $id)
+    {
+        $snack = Snack::findOrFail($id);
+        return view('snacks.edit', compact('snack'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $snack = Snack::findOrFail($id);
+        if($snack->update($request->all())) {
+            return redirect()->route('snacks.index')->with('sucesso', 'Lanche atualizado com sucesso!');    
+        }else{
+            return redirect()->route('snacks.index')->with('erro', 'Erro não foi possível atualizar o lanche.');
+        }
+    }
+
+    public function destroy(string $id)
+    {
+        $snack = Snack::findOrFail($id);
+        if($snack->delete()) {
+            return redirect()->back()->with('sucesso', 'Lanche deletado com sucesso!');    
+        }else{
+            return redirect()->back()->with('erro', 'Erro não foi possível deletar o lanche.');
+        }
+    }
+}
---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Ticket;
+use App\Models\Session;
+
+class TicketController extends Controller
+{
+    
+    public function index()
+    {
+        $tickets = Ticket::with(['session'])->get();
+        return view('tickets.index', compact('tickets'));
+    }
+
+    public function create()
+    {
+        $sessions = Session::all();
+        return view('tickets.create', compact('sessions'));
+    }
+
+    public function store(Request $request)
+    {
+        if(Ticket::create($request->all())) {
+            return redirect()->route('tickets.index')->with('sucesso', 'Ingresso criado com sucesso!');    
+        }else{
+            return redirect()->route('tickets.index')->with('erro', 'Erro não foi possível cadastrar o ingresso.');
+        }
+    }
+
+    public function show(string $id)
+    {
+        $ticket = Ticket::findOrFail($id);
+        return view('tickets.show', compact('ticket'));
+    }
+
+    public function edit(string $id)
+    {
+        $ticket = Ticket::findOrFail($id);
+        $sessions = Session::all();
+        return view('tickets.edit', compact('ticket', 'sessions'));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $ticket = Ticket::findOrFail($id);
+        if($ticket->update($request->all())) {
+            return redirect()->route('tickets.index')->with('sucesso', 'Ingresso atualizado com sucesso!');    
+        }else{
+            return redirect()->route('tickets.index')->with('erro', 'Erro não foi possível atualizar o ingresso.');
+        }
+    }
+
+    public function destroy(string $id)
+    {
+        $ticket = Ticket::findOrFail($id);
+        if($ticket->delete()) {
+            return redirect()->back()->with('sucesso', 'Ingresso deletado com sucesso!');    
+        }else{
+            return redirect()->back()->with('erro', 'Erro não foi possível deletar o ingresso.');
+        }
+    }
+}
