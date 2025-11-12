53e67b3217bcfba4ff1013980d0eb3061a23a4d3
Isa <isadora.silva112@aluno.ce.gov.br>
2025-11-12 07:22:44 -0300

    chore: adição do Laravel Breeze ao projeto

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 use App\Http\Controllers\SessionController;
@@ -7,8 +8,24 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\SnackController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::resource('movies', MovieController::class);
 Route::resource('rooms', RoomController::class);
 Route::resource('sessions', SessionController::class);
 Route::resource('tickets', TicketController::class);
 Route::resource('snacks', SnackController::class);
+
+require __DIR__.'/auth.php';
