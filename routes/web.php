c0c52987e91de1da63a670335193378c3e55e9f3
Isa <isadora.silva112@aluno.ce.gov.br>
2025-11-30 00:01:42 -0300

    refactor: atualização das rotas dos CRUDs e ajustes gerais no web.php

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,29 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
-use App\Http\Controllers\SessionController;
+use App\Http\Controllers\MovieSessionController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\SnackController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
     Route::resource('movies', MovieController::class);
     Route::resource('rooms', RoomController::class);
-    Route::resource('sessions', SessionController::class);
+    Route::resource('sessions', MovieSessionController::class);
     Route::resource('tickets', TicketController::class);
     Route::resource('snacks', SnackController::class);
 });
